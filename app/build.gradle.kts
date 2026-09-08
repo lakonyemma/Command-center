a6898c839b15080efd39a855f090c126c519b6c3
Lakony Emmanuel <lakonyemmanuel92@gmail.com>
2026-09-08 21:16:23 +0300

    Use cached stable debug keystore for Android builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,21 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        getByName("debug") {
+            storeFile = file("${System.getProperty("user.home")}/.android/debug.keystore")
+            storePassword = "android"
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+        }
+    }
+
+    buildTypes {
+        getByName("debug") {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+
     buildFeatures {
         compose = true
     }
